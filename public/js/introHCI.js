9469e7132a73819df71ea76729a93f8bd0387c9b
Byunggwan Lim <bg_lim@hotmail.com>
2017-02-15 23:02:50 -0800

    Done with lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
 	$('#colorBtn').click(randomizeColors);
 }
 
@@ -25,9 +24,20 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
-	console.log("User clicked on project " + idNumber);
+  $.get('/project/' + idNumber, addProject);
 }
+
+function addProject(result) {
+    console.log(result);
+
+    var projectHTML = '<class="thumbnail">' +
+    '<img src="' + result['image'] +'" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] + '</small></p></a>';
+
+    $('#' + 'project' + result['id'] + ' .details').append(projectHTML);
+    $('#' + 'project' + result['id'] + ' .details').append(result["summary"]);
+  }
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +45,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+  var temp = $.get('/palette/', addColor);
+
+}
+
+function addColor(result) {
+  var colors = result.colors["hex"];
+  $('body').css('background-color', colors[0]);
+  $('.thumbnail').css('background-color', colors[1]);
+  $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+  $('p').css('color', colors[3]);
+  $('.project img').css('opacity', .75);
 }
